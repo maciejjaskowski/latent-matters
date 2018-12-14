9bd410a93f44a987cae9fd75a0f4c932c39e6d2e
Maciej <maciej.jaskowski@gmail.com>
2018-12-14 13:21:15 +0100

    Working nicelYgit add latent_training_inspired*git add latent_training_inspired*

--- latent_training_inspired2.py
+++ latent_training_inspired2.py
@@ -23,10 +23,10 @@ class Net(nn.Module):
         super(Net, self).__init__()
         self.n_hidden_channels = n_hidden_channels
         self.conv1 = nn.Conv2d(n_input_channels, 20, kernel_size=3, dilation=1)
-        self.conv2 = nn.Conv2d(20, 30, kernel_size=3, dilation=1, stride=2)
+        self.conv2 = nn.Conv2d(20, 30, kernel_size=3, dilation=1, stride=1)
 
         self.bn = nn.BatchNorm2d(30)
-        self.conv3 = nn.Conv2d(30, self.n_hidden_channels, kernel_size=3, dilation=1, stride=2)  # Modelujemy 3 obiekty
+        self.conv3 = nn.Conv2d(30, self.n_hidden_channels, kernel_size=3, dilation=1, stride=1)  # Modelujemy 3 obiekty
 
 
         # self.fc2_dropout = nn.Dropout2d(p=0.5)
@@ -36,12 +36,10 @@ class Net(nn.Module):
 
         self.ran = torch.Tensor([range(80)] * 80)
 
-    def _softargmax(self, x4):
-        softmaxed = F.softmax(x4.reshape([-1, x4.shape[1], x4.shape[2]*x4.shape[3]]), dim=-1).reshape(x4.shape)  #, _stacklevel=5)
-
+    def _softargmax(self, x4, T=400.0):
+        softmaxed = F.softmax(T*x4.reshape([-1, x4.shape[1], x4.shape[2]*x4.shape[3]]), dim=-1).reshape(x4.shape)  #, _stacklevel=5)
         y_v = torch.stack([self.ran] * softmaxed.shape[1], dim=0)
         y_v = torch.stack([y_v] * softmaxed.shape[0], dim=0)
-
         x_v = torch.stack([self.ran.t()] * softmaxed.shape[1], dim=0)
         x_v = torch.stack([x_v] * softmaxed.shape[0], dim=0)
         assert (softmaxed.shape == y_v.shape), softmaxed.shape
@@ -51,34 +49,17 @@ class Net(nn.Module):
 
     def _encode(self, x):
         # print("SSS", x[0,0,40,:].mean())
-
         x2 = F.relu(self.conv1(x-0.33))
-        x3 = F.relu(self.bn(self.conv2(x2)))
+        # x3 = F.relu(self.bn(self.conv2(x2)))
+        x3 = F.relu(self.conv2(x2))
         dense = self.conv3(x3)  # No activation as we _softargmax the result
         dense_upscaled = F.interpolate(dense, size=(80, 80), mode='bilinear', align_corners=False)
         x_v, y_v, softmaxed = self._softargmax(dense_upscaled)
-
         assert list(x_v.shape)[1:] == [self.n_hidden_channels], (list(x_v.shape)[1:], [self.n_hidden_channels])
         xy = torch.stack([x_v, y_v], dim=2)
         assert list(xy.shape[1:]) == [self.n_hidden_channels, 2], "{} != {}".format(list(xy.shape[1:]), [self.n_hidden_channels, 2])
-        return xy, softmaxed
+        return xy, softmaxed, dense, dense_upscaled
 
-    def _combine_with_one_hot(self, res, i):
-        assert self.n_hidden_channels == 3, " A Temporary assumption"
-        one_hot = torch.Tensor(1.0 * np.array([[i == 0, i == 1, i == 2]] * res.shape[0]))
-        # print("SHAPE", one_hot.shape)
-        #
-        # print("SHAPE", res.shape)
-        r = torch.cat([res.reshape(-1,1), one_hot], dim=1)
-        # print("SHAPE", r.shape)
-        return r
-
-    def _decode(self, res, i):
-        r = self._combine_with_one_hot(res, i)
-
-        res1 = F.relu(self.fc1(r))
-        res2 = F.relu(self.fc2(res1))
-        return self.fc3(res2)
 
     def losses(self, keypoints1, map1, keypoints1_prev, img_change):
 
@@ -92,7 +73,7 @@ class Net(nn.Module):
                 if torch.mean(img_change[b]) > 0:
                     silhuette_consistency_loss.append(-torch.log(eps + torch.sum(map1[b,k,:,:] * img_change[b,:,:])))
 
-        delta = 15.0
+        delta = 3.0
         keypoint_variety_loss = torch.Tensor([0.0])
         for b in range(keypoints1.shape[0]):
             for i in range(keypoints1.shape[1]):
@@ -109,9 +90,9 @@ class Net(nn.Module):
         return keypoints_consistency_loss, silhuette_consistency_loss, keypoint_variety_loss[0]
 
     def forward(self, X):
-        keypoints1, map1 = self._encode(X['first'])
-        keypoints1_prev, _ = self._encode(X['first_prev'])
-        keypoints2, map2 = self._encode(X['second'])
+        keypoints1, map1, _, _ = self._encode(X['first'])
+        keypoints1_prev, _, _, _ = self._encode(X['first_prev'])
+        keypoints2, map2, _, _ = self._encode(X['second'])
 
         # img_change = torch.sum(((torch.abs(X['first'] - X['second']) > 0)).float(), dim=1)
         img_change = torch.sum(((torch.abs(X['first_prev'] - X['second']) > 0)).float(), dim=1)
@@ -151,7 +132,7 @@ def train(args, classification, model, device, train_loader, optimizer, epoch):
         keypoint_variety_loss = 0.01*output['keypoint_variety_loss']
 
         loss = loss_move + output['silhuette_consistency_loss'] + output['keypoint_variety_loss'] + keypoints_consistency_loss
-        loss = output['silhuette_consistency_loss'] + keypoint_variety_loss + keypoints_consistency_loss
+        loss = output['silhuette_consistency_loss'] + keypoint_variety_loss #+ keypoints_consistency_loss
         loss.backward()
         optimizer.step()
         if batch_idx % args.log_interval == 0:
@@ -181,33 +162,50 @@ def test(args, model, device, test_loader):
         test_loss, correct, len(test_loader.dataset),
         100. * correct / len(test_loader.dataset)))
 
+def single_image(i_obs):
+    # sample = iter(test_loader).next()
+    sample, _ = train_dataset.get_sample(0, i_obs, i_obs + 1)
+    res = model.forward({'first': sample['first'].unsqueeze(0), 'first_prev': sample['first_prev'].unsqueeze(0),
+                     'second': sample['second'].unsqueeze(0)})
+    png = sample['first']
+    xy, softmaxed, dense, dense_upscaled = model._encode(png.unsqueeze(0))
+    xy = xy.detach().numpy()
+    print("XY shape", xy.shape)
+    softmaxed = softmaxed.detach().numpy()
+    xy = np.stack([np.unravel_index(np.argmax(dense_upscaled.detach().numpy()[0][i]), (80, 80)) for i in range(6)])
+    xy = xy.reshape((1, 6, 2))
+    print("XY shape2", xy.shape)
+    x_r = xy[:, :, 0]
+    y_r = xy[:, :, 1]
+    r = np.zeros((80, 80, 3), np.uint8)
+    print(softmaxed[0, :, :, :])
+    attention = (cv2.resize(softmaxed[0, :, :, :].transpose([1, 2, 0]), (80, 80)) / softmaxed[0, :, :,
+
+                                                                                :].max() * 255).astype(np.uint8)
+    r[np.round(x_r).astype(np.int32), np.round(y_r).astype(np.int32), :] = [255, 0, 0]
+    print(np.array(png).dtype, attention.dtype, r.dtype)
+    repr = Image.fromarray(r).resize((320, 320))
+    attention = attention[:, :, :3] + attention[:, :, 3:]
+    print(attention.shape)
+    attention = Image.fromarray(attention).resize((320, 320))
+    print(np.array(repr))
+
+    res = np.maximum(np.array(torchvision.transforms.ToPILImage()(png).resize((320, 320))), np.array(repr))#, np.array(attention))
+    # res = np.maximum(np.array(repr), np.array(attention))
+    print(attention)
+    # res = np.array(attention)
+    Image.fromarray(res).save('game0_{:03d}.png'.format(i_obs))
+
 def play(train_dataset):
     game = train_dataset.game_data[0]
     for i_obs, obs in enumerate(game):
-        # sample = iter(test_loader).next()
-        sample, _ = train_dataset.get_sample(0, i_obs, i_obs+1)
-        res = model.forward({'first': sample['first'].unsqueeze(0), 'first_prev': sample['first_prev'].unsqueeze(0),
-                           'second': sample['second'].unsqueeze(0)})
+        single_image(i_obs)
 
-        xy, softmaxed = model._encode(train_dataset.transform(png).unsqueeze(0))
-        xy = xy.detach().numpy()
-        softmaxed = softmaxed.detach().numpy()
-        y_r = xy[:, :, 0]
-        x_r = xy[:, :, 1]
-        r = np.zeros((80, 80, 3), np.uint8)
-        print(softmaxed[0, :, :, :])
-        attention = (cv2.resize(softmaxed[0, :, :, :].transpose([1,2,0]), (80, 80)) / softmaxed[0, :, :, :].max() * 255).astype(np.uint8)
-        r[np.round(x_r).astype(np.int32), np.round(y_r).astype(np.int32), :] = [255, 0, 0]
-        print(np.array(png).dtype, attention.dtype)
-        repr = Image.fromarray(r).resize((320, 320))
-        attention = Image.fromarray(attention).resize((320,320))
-        print(np.array(repr))
-        #res = np.maximum(np.array(png), np.array(repr), np.array(attention))
-        res = np.maximum(np.array(repr), np.array(attention))
-        Image.fromarray(res).save('game0_{:03d}.png'.format(i_obs))
 
 # Max diff ustawic na 2 lub 3 ?
 # Bilinear w innym miejscu ?
+# Zrobić "zwykly" attention do oszacowania img_change, po to zeby suma "map" dodawala sie do img_change ?
+# Cos ten softargmax nie dziala tak jak powinien (porownaj z argmax)
 
 
 if __name__ == '__main__':
@@ -242,7 +240,7 @@ if __name__ == '__main__':
         root=data_path,
         n_games=100,
         min_diff=1,
-        max_diff=3
+        max_diff=2
     )
     train_loader = DataLoader(train_dataset, batch_size=64,num_workers=5,shuffle=False,
     )
@@ -253,7 +251,7 @@ if __name__ == '__main__':
 
 
     classification = False
-    model = Net(n_input_channels=3, n_hidden_channels=3).to(device)
+    model = Net(n_input_channels=3, n_hidden_channels=6).to(device)
 
     optimizer = optim.Adam(model.parameters(), lr=args.lr)
 
