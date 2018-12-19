9b286e7951f27f75a3895da6c849ca350beb6e7d
Maciej <maciej.jaskowski@gmail.com>
2018-12-19 11:49:28 +0100

    More robust video creation.

--- create_video.py
+++ create_video.py
@@ -1,24 +1,33 @@
 import os
-epoch = 218
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--epoch", required=True, type=int)
+parser.add_argument('--keypoints', required=True, type=int)
+args = parser.parse_args()
+
+epoch = args.epoch
+
+
 outs = []
-for att in range(6):
-    out = "epoch{epoch:05d}_att_{att:03d}.mp4".format(epoch=epoch, att=att)
-    os.system("ffmpeg -r 1 -i epoch{epoch:05d}_game0_att_{att:03d}_%03d.png -c:v libx264 -vf fps=25 -pix_fmt yuv420p {out};"
+for att in range(args.keypoints):
+    out = "epoch{epoch:05d}/att_{att:03d}.mp4".format(epoch=epoch, att=att)
+    os.system("ffmpeg -r 1 -i epoch{epoch:05d}/game0_att_{att:03d}_%03d.png -c:v libx264 -vf fps=25 -pix_fmt yuv420p {out};"
           .format(epoch=epoch, att=att, out=out))
     outs.append(out)
 
-out = "epoch{epoch:05d}_attentions_png.mp4".format(epoch=epoch)
-os.system("ffmpeg -r 1 -i epoch{epoch:05d}_game0_attentions_png_%03d.png -c:v libx264 -vf fps=25 -pix_fmt yuv420p {out};"
+out = "epoch{epoch:05d}/attentions_png.mp4".format(epoch=epoch)
+os.system("ffmpeg -r 1 -i epoch{epoch:05d}/game0_attentions_png_%03d.png -c:v libx264 -vf fps=25 -pix_fmt yuv420p {out};"
       .format(epoch=epoch, out=out))
 outs.append(out)
 
-out = "epoch{epoch:05d}_argmaxxy_png.mp4".format(epoch=epoch)
-os.system("ffmpeg -r 1 -i epoch{epoch:05d}_game0_argmaxxy_png_%03d.png -c:v libx264 -vf fps=25 -pix_fmt yuv420p;"
+out = "epoch{epoch:05d}/argmaxxy_png.mp4".format(epoch=epoch)
+os.system("ffmpeg -r 1 -i epoch{epoch:05d}/game0_argmaxxy_png_%03d.png -c:v libx264 -vf fps=25 -pix_fmt yuv420p;"
       .format(epoch=epoch))
 outs.append(out)
 
-out = "epoch{epoch:05d}_xy_png.mp4".format(epoch=epoch)
-os.system("ffmpeg -r 1 -i epoch{epoch:05d}_game0_xy_png_%03d.png -c:v libx264 -vf fps=25 -pix_fmt yuv420p {out};"
+out = "epoch{epoch:05d}/xy_png.mp4".format(epoch=epoch)
+os.system("ffmpeg -r 1 -i epoch{epoch:05d}/game0_xy_png_%03d.png -c:v libx264 -vf fps=25 -pix_fmt yuv420p {out};"
       .format(epoch=epoch, out=out))
 outs.append(out)
 
